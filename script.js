868ec2b8ad7b30720f42ddea161e3286987b006b
m-j0401 <mehul.jain2206@gmail.com>
2026-04-10 19:24:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,17 +10,10 @@ btn.addEventListener("click",(e)=>{
 
 	e.preventDefault();
 	alert(`Logged in as ${input_one.value}`);
-// 	let obj={
-// 	username:input_one.value,
-// 	password:input_two.value
-// }
-// let json_obj=JSON.stringify(obj);
+
 if(check.checked)
 {
-	//alert(`Logged in as ${input_one.value}`);
 	localStorage.setItem(input_two.value,input_one.value);
-	// exist_btocn.id="existing";
-	// exist_btn.innerText="Login as existing user";
 	exist_btn.style.display="block";
 }
 	else{
